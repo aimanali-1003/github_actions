9548508d1b3730a2548a3eac75931008bfecae4a
aimanali-1003 <aiman.ali@devsinc.com>
2022-12-20 19:25:46 +0500

    rubocop changes new

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,7 +7,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-50.times do |x|
-    Post.create(title: Faker::Lorem.sentences(number: 1),
-        body: Faker::Lorem.paragraph(sentence_count: 5))
+50.times do |_x|
+  Post.create(title: Faker::Lorem.sentences(number: 1),
+              body: Faker::Lorem.paragraph(sentence_count: 5))
 end
